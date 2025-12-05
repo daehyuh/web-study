52d715c623fa6b155a5af9e30ca7e2bad194c678
daehyuh <rkdeown10@naver.com>
2025-12-05 18:41:08 +0900

    fix : seacurity disabled

--- spring_project1/src/main/java/com/canesblack/spring_project1/SpringProject1Application.java
+++ spring_project1/src/main/java/com/canesblack/spring_project1/SpringProject1Application.java
@@ -3,7 +3,7 @@ package com.canesblack.spring_project1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class SpringProject1Application {
 	
 	public static void main(String[] args) {
